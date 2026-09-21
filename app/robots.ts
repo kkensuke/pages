3cc55c5b966a63bd7d576b7614b5a8a7221c3aee
Kensuke Kamisoyama <47603428+kkensuke@users.noreply.github.com>
2026-09-22 07:26:07 +0900

    chore: remove retired owner and shop routes from robots

--- app/robots.ts
+++ app/robots.ts
@@ -7,15 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: [
-          '/owner',
-          '/owner/*',
-          '/api/',
-          '/login',
-          // Uncomment if you enable products feature
-          // '/checkout',
-          // '/success',
-        ],
+        disallow: ['/api/'],
       },
     ],
     sitemap: `${SITE_CONFIG.url}/sitemap.xml`,
